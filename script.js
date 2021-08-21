99e274ed0f91e3781551581661820817c1c94c0f
azlaan2605 <azzuazlaan@gmail.com>
2021-08-21 10:43:45 +0530

    dom1

---
+++ script.js
@@ -0,0 +1,30 @@
+// para.style.color="blue"
+
+// para.style.cssText="background-color:blue ;color:white"; 
+
+// function oneclick(){
+//     console.log("button clicked!");
+// }
+
+
+
+
+// function oneclick(){
+    
+//     para.classList.add("edit")
+//     console.log("button clicked");
+// }
+
+// //Inner text-
+// let paragraph= document.getElementById("para")
+// console.log(paragraph.innerText);
+
+
+// TextContent
+// let paragraph= document.getElementById("para")
+// console.log(paragraph.textContent);
+
+
+// innerHTML
+// let paragraph= document.getElementById("para")
+// console.log(paragraph.innerHTML);
